2420f21d88f5a275d1480390a1aea6162998b321
fralonra <fralonra@aliyun.com>
2019-12-11 18:21:38 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+const path = require('path')
 const Felid = require('felid')
 
 const app = new Felid()
 
+app.plugin(require('felid-serve'), {
+  root: path.resolve(__dirname, '../static'),
+})
+app.plugin(require('felid-handlebars'), {
+  root: path.resolve(__dirname, 'views')
+})
+
+app.get('/public/:file', (req, res) => {
+  res.serve(req.params.file)
+})
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index.hbs', {
+    logoPath: 'public/logo.svg',
+    title: 'Felid Now'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('done')
 })
 
 module.exports = app.lookup()
